7b79eade1b379d14b1559da95857276dd80200ec
MGabrielaCabrera <gabrielacabreracastellano@gmail.com>
2026-05-24 13:09:21 +0200

    Packages files created

---
+++ cfs_algn_test_pkg.sv
@@ -0,0 +1,20 @@
+//Package for the tests files
+// ifndef is used to prevent multiple inclusion of the same package
+`ifndef CFS_ALGN_TEST_PKG_SV
+    `define CFS_ALGN_TEST_PKG_SV
+    `include "uvm_macros.svh"
+    `include "cfs_algn_pkg.sv"
+
+    package cfs_algn_test_pkg;
+        import uvm_pkg::*;
+        import cfs_algn_pkg::*;
+
+        `include "cfs_algn_test_base.sv"
+        `include "cfs_algn_test_reg_access.sv"
+
+
+
+
+    endpackage;
+
+`endif
