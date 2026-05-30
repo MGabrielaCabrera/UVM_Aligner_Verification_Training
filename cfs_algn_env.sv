301867e2b0ca947307a0010c4d959553a4026897
MGabrielaCabrera <gabrielacabreracastellano@gmail.com>
2026-05-30 15:40:58 +0200

    Connecting the interface to the agent using the DB

--- cfs_algn_env.sv
+++ cfs_algn_env.sv
@@ -1,6 +1,8 @@
 `ifndef CFS_ALGN_ENV_SV
     `define CFS_ALGN_ENV_SV
         class cfs_algn_env extends uvm_env;
+
+            cfs_apb_agent apb_agent;
     
             // Mandatory for all the test classes to have the UVM macro to register
             // the class with the factory, enabling the core UVM infrastructure 
@@ -10,6 +12,12 @@
     
             function new(string name = "", uvm_component parent);
                 super.new(name, parent);
+            endfunction
+
+            virtual function void build_phase(uvm_phase phase);
+                super.build_phase(phase);
+                // We create the APB agent and set it as a child of the environment
+                apb_agent = cfs_apb_agent::type_id::create("apb_agent", this);
             endfunction
         endclass
 `endif
