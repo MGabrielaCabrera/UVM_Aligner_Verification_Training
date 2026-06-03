8fc5c6bc11fbf30ee915ebd2e1794303bcb38beb
MGabrielaCabrera <gabrielacabreracastellano@gmail.com>
2026-06-03 15:14:50 +0200

    Sequence simpler created

---
+++ cfs_apb_sequence_simple.sv
@@ -0,0 +1,22 @@
+`ifndef CFS_APB_SEQUENCE_SIMPLE_SV
+    `define CFS_APB_SEQUENCE_SIMPLE_SV
+
+    class cfs_apb_sequence_simple extends cfs_apb_sequence_base;
+        rand cfs_apb_item_drv item;
+
+        `uvm_object_utils(cfs_apb_sequence_simple)
+
+        function new(string name = "");
+            super.new(name);
+            
+            item = cfs_apb_item_drv::type_id::create("item");
+        endfunction
+
+        task body();
+            // Start the sequence by sending the item to the sequencer
+            start_item(item);
+            finish_item(item);
+        endtask
+
+    endclass
+`endif
