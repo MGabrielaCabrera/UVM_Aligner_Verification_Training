9c9a21ad324130b8aabb5b338ec6dcac203954c0
MGabrielaCabrera <gabrielacabreracastellano@gmail.com>
2026-06-03 14:56:01 +0200

    The driver and the sequencer have been instantiated

--- cfs_apb_agent.sv
+++ cfs_apb_agent.sv
@@ -2,11 +2,14 @@
     `define CFS_APB_AGENT_SV
 
     class cfs_apb_agent extends uvm_agent;
-        `uvm_component_utils(cfs_apb_agent)
-
 
         cfs_apb_agent_config agent_config;
+        
+        // Sequencer handler and driver handler
+        cfs_apb_sequencer sequencer;
+        cfs_apb_driver driver;
 
+        `uvm_component_utils(cfs_apb_agent)
 
         function new(string name = "", uvm_component parent);
             super.new(name, parent);
@@ -16,6 +19,13 @@
             super.build_phase(phase);
             // We create the configuration object and set it as a child of the agent
             agent_config = cfs_apb_agent_config::type_id::create("agent_config", this);
+            
+            // We check if the agent is active or passive, and we create the sequencer
+            // and driver only if it's active
+            if (agent_config.get_active_passive() == UVM_ACTIVE) begin
+                sequencer = cfs_apb_sequencer::type_id::create("sequencer", this);
+                driver = cfs_apb_driver::type_id::create("driver", this);
+            end
         endfunction
 
         virtual function void connect_phase(uvm_phase phase);
@@ -27,6 +37,11 @@
             end
             else begin
                 agent_config.set_vif(vif);
+            end
+
+            // We connect the driver and the sequencer only if the agent is active
+            if (agent_config.get_active_passive() == UVM_ACTIVE) begin
+                driver.seq_item_port.connect(sequencer.seq_item_export);
             end
         endfunction
 
