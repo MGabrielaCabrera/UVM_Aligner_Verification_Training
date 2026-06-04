a834243255b2471417a94bae3ce8da3b81e41f5b
MGabrielaCabrera <gabrielacabreracastellano@gmail.com>
2026-06-04 15:28:13 +0200

    The sequence random has been created and instantiated in the test

--- cfs_apb_pkg.sv
+++ cfs_apb_pkg.sv
@@ -17,6 +17,7 @@
         `include "cfs_apb_sequence_base.sv"
         `include "cfs_apb_sequence_simple.sv"
         `include "cfs_apb_sequence_rw.sv"
+        `include "cfs_apb_sequence_random.sv"
         `include "cfs_apb_driver.sv"
         `include "cfs_apb_agent_config.sv"
         `include "cfs_apb_agent.sv"
