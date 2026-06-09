593e8cc227f4cc84a38939fe945ace0dd0df7c90
MGabrielaCabrera <gabrielacabreracastellano@gmail.com>
2026-06-09 09:07:32 +0200

    Creating the monitor item and restructuring the item base

--- cfs_apb_pkg.sv
+++ cfs_apb_pkg.sv
@@ -13,6 +13,7 @@
         `include "cfs_apb_types.sv"
         `include "cfs_apb_item_base.sv"
         `include "cfs_apb_item_drv.sv"
+        `include "cfs_apb_item_mon.sv"
         `include "cfs_apb_sequencer.sv"
         `include "cfs_apb_sequence_base.sv"
         `include "cfs_apb_sequence_simple.sv"
