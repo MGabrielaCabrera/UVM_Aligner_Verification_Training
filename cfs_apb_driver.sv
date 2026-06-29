42bd7303d173972a8f28900fd757bea9a537bed8
MGabrielaCabrera <gabrielacabreracastellano@gmail.com>
2026-06-29 17:39:33 +0200

    reset handler method of the apb driver

--- cfs_apb_driver.sv
+++ cfs_apb_driver.sv
@@ -1,11 +1,14 @@
 `ifndef CFS_APB_DRIVER_SV
     `define CFS_APB_DRIVER_SV
 
-    class cfs_apb_driver extends uvm_driver#(.REQ(cfs_apb_item_drv));
+    class cfs_apb_driver extends uvm_driver#(.REQ(cfs_apb_item_drv)) implements cfs_apb_reset_handler;
 
         // Declaring agent config class to have access to the virtual 
         // interface through a pointer (in the agent class)
         cfs_apb_agent_config agent_config;
+
+        // Process for driver_transactions() task
+        protected process process_drive_transactions;
 
         `uvm_component_utils(cfs_apb_driver)
  
@@ -13,15 +16,54 @@
             super.new(name, parent);
         endfunction
 
+        // Task for waiting the reset to end (synchronous)
+        virtual task wait_reset_end();
+            agent_config.wait_reset_end();
+        endtask
+
         virtual task run_phase(uvm_phase phase);
-            driver_transactions();
+            forever begin
+                fork
+                    begin
+                        wait_reset_end();
+                        driver_transactions();
+                        
+                        disable fork;
+                    end
+                join
+            end
         endtask
 
         protected virtual task driver_transactions();
+            fork
+                begin
+                    process_drive_transactions = process::self();
+
+                    forever begin
+
+                        cfs_apb_item_drv item;
+
+                        seq_item_port.get_next_item(item);
+                        
+                        driver_transaction(item);
+                        
+                        seq_item_port.item_done();
+                    end
+                end
+            join 
+        endtask
+
+        // Function to handle the reset
+        virtual function void handler_reset(uvm_phase phase);
             // We get the virtual interface from the agent config
             // using get_vif() to protect the encapsulation
             // of the virtual interface inside the agent config class
             cfs_apb_vif vif = agent_config.get_vif();
+
+            if(process_drive_transactions != null) begin
+                process_drive_transactions.kill();
+                process_drive_transactions = null;
+            end
 
             // Initialize the signals
             vif.paddr <= '0;
@@ -29,17 +71,7 @@
             vif.pwdata <= '0;
             vif.psel <= 0;
             vif.penable <= 0;
-
-            forever begin
-                cfs_apb_item_drv item;
-
-                seq_item_port.get_next_item(item);
-                
-                driver_transaction(item);
-                
-                seq_item_port.item_done();
-            end
-        endtask
+        endfunction
 
         protected virtual task driver_transaction(cfs_apb_item_drv item);
             cfs_apb_vif vif = agent_config.get_vif();
