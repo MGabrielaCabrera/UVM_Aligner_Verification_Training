531e4a77484edd4785520937c1b3a07574f951bd
MGabrielaCabrera <gabrielacabreracastellano@gmail.com>
2026-06-30 14:48:11 +0200

    Adding the reset handler of the apb monitor

--- cfs_apb_monitor.sv
+++ cfs_apb_monitor.sv
@@ -1,7 +1,7 @@
 `ifndef CFS_APB_MONITOR_SV
     `define CFS_APB_MONITOR_SV
 
-    class cfs_apb_monitor extends uvm_monitor;
+    class cfs_apb_monitor extends uvm_monitor implements cfs_apb_reset_handler;
 
         // Declaring agent config class to have access to the virtual 
         // interface through a pointer (in the agent class)
@@ -10,6 +10,9 @@
         // UVM Analysis port to send the collected transactions 
         // to other components
         uvm_analysis_port#(cfs_apb_item_mon) output_port;
+
+        // Process for collect_Transactions() task
+        protected process process_collect_transactions;
 
         `uvm_component_utils(cfs_apb_monitor)
 
@@ -20,13 +23,32 @@
         endfunction
         
         virtual task run_phase(uvm_phase phase);
-            collect_transactions();
+            forever begin
+                fork
+                    begin
+                        wait_reset_end();
+                        collect_transactions();
+                        disable fork;
+                    end
+                join
+            end
+        endtask
+
+        // Task for waiting the reset to end (synchronous)
+        virtual task wait_reset_end();
+            agent_config.wait_reset_end();
         endtask
 
         protected virtual task collect_transactions();
-           forever begin
-            collect_transaction();
-           end
+           fork
+                begin
+                    process_collect_transactions = process::self();
+
+                    forever begin
+                        collect_transaction();
+                    end
+                end
+           join
         endtask
 
         protected virtual task collect_transaction();
@@ -81,5 +103,12 @@
             // Waiting one clock cycle before exiting the task
             @(posedge vif.pclk);
         endtask
+
+        virtual function void handler_reset(uvm_phase phase);
+           if(process_collect_transactions != null) begin
+                process_collect_transactions.kill();
+                process_collect_transactions = null;
+            end
+        endfunction
     endclass
 `endif
