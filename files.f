d2aecd4833da6b64b28c4383c42dedab10e0fbcc
MGabrielaCabrera <gabrielacabreracastellano@gmail.com>
2026-07-15 16:58:18 +0200

    files.f with all the includes pkg files

---
+++ files.f
@@ -0,0 +1,10 @@
++incdir+./agents/apb
++incdir+./agents/apb/sequences
++incdir+./env
++incdir+./tests
+
+$UVM_HOME/src/uvm_pkg.sv
+./agents/apb/cfs_apb_pkg.sv
+./env/cfs_algn_env.sv
+./tests/cfs_algn_test_pkg.sv
+./tb/testbench.sv
