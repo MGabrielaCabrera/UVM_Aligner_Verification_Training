fcd443b855744e2981d30bbbe4ed36ccdae9aa74
MGabrielaCabrera <gabrielacabreracastellano@gmail.com>
2026-07-16 16:47:13 +0200

    Creating the md interface

--- env/cfs_algn_pkg.sv
+++ env/cfs_algn_pkg.sv
@@ -2,10 +2,12 @@
     `define CFS_ALGN_PKG_SV
     `include "uvm_macros.svh"
     `include "cfs_apb_pkg.sv"
+    `include "cfs_md_pkg.sv"
 
     package cfs_algn_pkg;
         import uvm_pkg::*;
         import cfs_apb_pkg::*;
+        import cfs_md_pkg::*;
         `include "cfs_algn_env.sv"
 
     endpackage;
