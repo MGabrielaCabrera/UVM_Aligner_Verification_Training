38d159ac485fa7fb9509fa30a41a4f8416308f22
MGabrielaCabrera <gabrielacabreracastellano@gmail.com>
2026-07-17 16:09:25 +0200

    MD agent cobfig created

--- tb/testbench.sv
+++ tb/testbench.sv
@@ -10,6 +10,13 @@ module testbench();
 
   // Instance of the APB interface
   cfs_apb_if apb_if(.pclk(clk));
+
+  // Instance of the MD RX interface
+  cfs_md_if#(32) md_rx_if(.clk(clk));
+
+  // Instance of the MD TX interface
+  cfs_md_if#(32) md_tx_if(.clk(clk));
+
   
   initial begin
     clk = 0;
@@ -25,6 +32,9 @@ module testbench();
     #30ns;
     apb_if.preset_n = 1;
   end
+
+  assign md_rx_if.reset_n = apb_if.preset_n;
+  assign md_tx_if.reset_n = apb_if.preset_n;
   
 
   initial begin
@@ -38,8 +48,12 @@ module testbench();
     // The second argument is the instance name of the component where the object will be used,
     // The third argument is the field name, which is a string that can be used to identify the object in the component
     // The fourth argument is the object being set, in this case the APB interface instance
-      uvm_config_db#(virtual cfs_apb_if)::set(null, "uvm_test_top.env.apb_agent", "vif", apb_if);
-      
+    uvm_config_db#(virtual cfs_apb_if)::set(null, "uvm_test_top.env.apb_agent", "vif", apb_if);
+
+    // MD interface to the database
+    uvm_config_db#(virtual cfs_md_if#(32))::set(null, "uvm_test_top.env.md_rx_agent", "vif", md_rx_if);
+    uvm_config_db#(virtual cfs_md_if#(32))::set(null, "uvm_test_top.env.md_tx_agent", "vif", md_tx_if);
+
     // Run the test
     //The test name can be passed as an argument when running the simulation
     // +access+r +UVM_TESTNAME=cfs_algn_test_reg_access +UVM_MAX_QUIT_COUNT=1 -coverage_functional
@@ -56,7 +70,21 @@ module testbench();
     .pwdata(apb_if.pwdata),
     .pready(apb_if.pready),
     .prdata(apb_if.prdata),
-    .pslverr(apb_if.pslverr)
+    .pslverr(apb_if.pslverr),
+
+    .md_rx_valid(md_rx_if.valid),
+    .md_rx_data(md_rx_if.data),
+    .md_rx_ready(md_rx_if.ready),
+    .md_rx_offset(md_rx_if.offset),
+    .md_rx_size(md_rx_if.size),
+    .md_rx_err(md_rx_if.err),
+
+    .md_tx_valid(md_tx_if.valid),
+    .md_tx_data(md_tx_if.data),
+    .md_tx_ready(md_tx_if.ready),
+    .md_tx_offset(md_tx_if.offset),
+    .md_tx_size(md_tx_if.size),
+    .md_tx_err(md_tx_if.err)
   );
   
 endmodule
