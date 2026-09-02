6b05fa4e75eee4360ad5918c71dad2d86e63c647
MGabrielaCabrera <gabrielacabreracastellano@gmail.com>
2026-09-02 12:36:22 +0200

    New assertions (data,err and size constants when ready is high

--- agents/md/cfs_md_if.sv
+++ agents/md/cfs_md_if.sv
@@ -49,7 +49,77 @@
         VALID_HIGH_UNTIL_READY_A : assert property(valid_high_until_ready_p) else
             $error("valid signal did not stay high until ready became high");
 
+        // 4. Data is valid while valid is high
+        property unknown_data_valid_p;
+            @(posedge clk) disable iff(!reset_n || !has_checks)
+            valid |-> $isunknown(data) == 1; // When valid is high, data must not have unknown values.
+        endproperty
 
+        UNKNOWN_DATA_VALID_A : assert property(unknown_data_valid_p) else
+            $error("Data signal has unknown values while valid is high");
+        
+        // 5. Data must remain constant until ready is high
+        property data_stable_until_ready_p;
+            @(posedge clk) disable iff(!reset_n || !has_checks)
+            valid |-> $stable(data) until ready == 1; // When valid is high, data must remain constant until ready is high.
+        endproperty
 
+        DATA_STABLE_UNTIL_READY_A : assert property(data_stable_until_ready_p) else
+            $error("Data signal did not remain constant until ready became high");
+
+        // 6. Offset is valid while valid is high
+        property unknown_offset_valid_p;
+            @(posedge clk) disable iff(!reset_n || !has_checks)
+            valid |-> $isunknown(offset) == 1; // When valid is high, offset must not have unknown values.
+        endproperty
+
+        UNKNOWN_OFFSET_VALID_A : assert property(unknown_offset_valid_p) else
+            $error("Offset signal has unknown values while valid is high");
+
+        // 7. Offset must remain constant until ready is high
+        property offset_stable_until_ready_p;
+            @(posedge clk) disable iff(!reset_n || !has_checks)
+            valid |-> $stable(offset) until ready == 1; // When valid is high, offset must remain constant until ready is high.
+        endproperty
+
+        OFFSET_STABLE_UNTIL_READY_A : assert property(offset_stable_until_ready_p) else
+            $error("Offset signal did not remain constant until ready became high");
+
+        // 8. Size is valid while valid is high
+        property unknown_size_valid_p;
+            @(posedge clk) disable iff(!reset_n || !has_checks)
+            valid |-> $isunknown(size) == 1; // When valid is high, size must not have unknown values.  
+        endproperty
+
+        UNKNOWN_SIZE_VALID_A : assert property(unknown_size_valid_p) else
+            $error("Size signal has unknown values while valid is high");
+
+        // 9. Size must remain constant until ready is high
+        property size_stable_until_ready_p;
+            @(posedge clk) disable iff(!reset_n || !has_checks)
+            valid |-> $stable(size) until ready == 1; // When valid is high, size must remain constant until ready is high. 
+        endproperty
+
+        SIZE_STABLE_UNTIL_READY_A : assert property(size_stable_until_ready_p) else
+            $error("Size signal did not remain constant until ready became high");
+        
+        // 11. Err is valid when valid and ready are high
+        property err_valid_p;
+            @(posedge clk) disable iff(!reset_n || !has_checks)
+            valid && ready |-> $isunknown(err) == 1; // When valid and ready are high, err must not have unknown values.
+        endproperty
+
+        ERR_VALID_A : assert property(err_valid_p) else
+            $error("Err signal has unknown values when valid and ready are high");
+
+        // 14. Ready is valid when valid is high
+        property ready_valid_p;
+            @(posedge clk) disable iff(!reset_n || !has_checks)
+            valid |-> $isunknown(ready) == 1; // When valid is high, ready must not have unknown values.
+        endproperty
+
+        READY_VALID_A : assert property(ready_valid_p) else
+            $error("Ready signal has unknown values when valid is high");
+            
     endinterface
 `endif
