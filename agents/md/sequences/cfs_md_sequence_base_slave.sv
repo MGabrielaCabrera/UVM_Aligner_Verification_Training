c230236095e6164dba2535e666962dd0701d9eba
MGabrielaCabrera <gabrielacabreracastellano@gmail.com>
2026-09-13 12:30:58 +0200

    Error in the p_sequencer solved

--- agents/md/sequences/cfs_md_sequence_base_slave.sv
+++ agents/md/sequences/cfs_md_sequence_base_slave.sv
@@ -4,7 +4,7 @@
     class cfs_md_sequence_base_slave extends cfs_md_sequence_base#(.ITEM_DRV(cfs_md_item_drv_slave));
 
         // Declaring p_sequencer using a macro
-        `uvm_declare_p_sequencer(cfs_md_sequencer_base_slave)
+        `uvm_declare_p_sequencer(cfs_md_sequencer_slave_base)
 
         `uvm_object_utils(cfs_md_sequence_base_slave)
 
