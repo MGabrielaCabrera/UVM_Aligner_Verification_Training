9f063644913df839d5b2f5b668eb1f32d47995a2
MGabrielaCabrera <gabrielacabreracastellano@gmail.com>
2026-09-18 11:28:39 +0200

    Extended agent config file was created and the apb agent config has been adapted

---
+++ agents/uvm_ext/uvm_ext_pkg.sv
@@ -0,0 +1,13 @@
+`ifndef UVM_EXT_PKG_SV
+    `define UVM_EXT_PKG_SV
+
+    `include "uvm_macros.svh"
+
+    package uvm_ext_pkg;
+        import uvm_pkg::*;
+
+        `include "uvm_ext_agent_config.sv"
+        
+
+    endpackage
+`endif
