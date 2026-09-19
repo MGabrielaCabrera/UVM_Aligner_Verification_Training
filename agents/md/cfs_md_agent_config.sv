e4053a1201809e59573831a1b2a160feb38b6d58
MGabrielaCabrera <gabrielacabreracastellano@gmail.com>
2026-09-19 15:24:05 +0200

    The md agent config has been adapted

--- agents/md/cfs_md_agent_config.sv
+++ agents/md/cfs_md_agent_config.sv
@@ -1,22 +1,9 @@
 `ifndef CFS_MD_AGENT_CONFIG_SV
     `define CFS_MD_AGENT_CONFIG_SV
 
-    class cfs_md_agent_config#(int unsigned DATA_WIDTH = 32) extends uvm_component;
+    class cfs_md_agent_config#(int unsigned DATA_WIDTH = 32) extends uvm_ext_agent_config#(.VIRTUAL_INTF(virtual cfs_md_if#(DATA_WIDTH)));
+
         typedef virtual cfs_md_if#(DATA_WIDTH) cfs_md_vif;
-
-        // Virtual interface
-        local cfs_md_vif vif;
-
-        // An agent is active when it has a driver, and passive when it doesn't
-        // have a driver. This is useful to determine if the agent will drive
-        // the signals or just monitor them.
-        local uvm_active_passive_enum active_passive;
-
-        //Switch to enable the checks
-        local bit has_checks;
-
-        //Switch to enable coverage
-        local bit has_coverage;
 
         // Delay used when detecting start of an MD transaction in the monitor
         local time sample_delay_start_tr;
@@ -30,45 +17,18 @@
         function new(string name = "", uvm_component parent);
             super.new(name, parent);
 
-            active_passive = UVM_ACTIVE; // By default, we set the agent to active
-            has_checks = 1; // By default, we enable the checks
-            has_coverage =1;
             sample_delay_start_tr = 1ns; // By default, we set the sample delay to 0
             stuck_threshold = 1000;
         endfunction
 
-
-        virtual function cfs_md_vif get_vif();
-            return vif;
-        endfunction
-
         virtual function void set_vif(cfs_md_vif value);
-            // To prevent it is only set once, we check if it is already set before assigning the value
-            if (vif == null) begin
-                vif = value;
-
-                // Sync with the has_checks of the interface
-                set_has_checks(get_has_checks());
-            end
-            else begin
-                `uvm_fatal("ALGORITHM_ISSUE", "Trying to set the MD virtual interface more than once")
-            end
-        endfunction
-
-        virtual function uvm_active_passive_enum get_active_passive();
-            return active_passive;  
-        endfunction
-
-        virtual function void set_active_passive(uvm_active_passive_enum value);
-            active_passive = value;
-        endfunction
-
-        virtual function bit get_has_checks();
-            return has_checks;
+            super.set_vif(value); // UVM_ext parent
+            // Sync with the has_checks of the interface
+            set_has_checks(get_has_checks());
         endfunction
 
         virtual function void set_has_checks(bit value);
-            has_checks = value;
+            super.set_has_checks(value); // UVM_ext parent
 
             // Mechanisism to ensure that the has_checks value is syncronize
             // with the interface has_check variable
@@ -91,14 +51,6 @@
 
         virtual function void set_sample_delay_start_tr(time value);
             sample_delay_start_tr = value;
-        endfunction
-
-        virtual function bit get_has_coverage();
-            return has_coverage;
-        endfunction
-
-        virtual function void set_has_coverage(bit value);
-            has_coverage = value;
         endfunction
 
         // Task for waiting the reset to start (asynchronous)
